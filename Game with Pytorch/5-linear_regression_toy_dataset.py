62356385bee604ec92ee978583a0b09b9678596a
Iman <imankhanijazani@gmail.com>
2022-10-28 23:39:55 +0330

    init linear reg for a toy dataset

---
+++ Game with Pytorch/5-linear_regression_toy_dataset.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+'''
+1- model building
+2- loss and optimizer
+3- training loop
+'''
+
